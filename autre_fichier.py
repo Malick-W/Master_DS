33edcad67b1fd6fb993d2e80d2f8baa2f38af8dd
Malick <wmalick01@yahoo.fr>
2019-09-22 01:43:40 +0200

    Ajout de code à autre_fichier.py

---
+++ autre_fichier.py
@@ -0,0 +1,34 @@
+# /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import threading
+import time
+
+## Exercice 1
+# question1
+
+class MaTache(threading.Thread):
+    def __init__(self, n,m,s):
+            threading.Thread.__init__(self)
+                    self.n = n
+                            self.m = m
+                                    self.s = s
+
+                                        def run(self):
+                                                while True:
+                                                        	for i in range(self.n):
+                                                        	        		print(self.m)
+                                                        	        		        	time.sleep(self.s)
+
+                                                        	        		        	if __name__ == '__main__':
+                                                        	        		        	  matache = MaTache(3,"Hello World", 4)
+                                                        	        		        	    matache.start()
+
+
+                                                        	        		        	    # question 2
+
+
+
+
+
+                                                        	        		        	    
